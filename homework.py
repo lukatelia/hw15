9fc847e6fe90a892519bc7284c8f07e9114e9586
lukatelia <138887175+lukatelia@users.noreply.github.com>
2023-08-30 17:35:59 +0400

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,20 @@
+#pirveli davaleba
+
+country = "saqartvelo"
+print(country[5:10])
+print(country[6:10])
+print(country[4:7])
+print(country[3:6])
+print(country[7:9])
+
+#second homework
+
+country = ["georgia", "switzerland" , "Australia" , "canada" ]
+print(country[0:2])
+print(country[0:3])
+print(country[0:4])
+print(country[1:2])
+print(country[1:3])
+print(country[1:4])
+print(country[2:3])
+print(country[3:4])
